298bb5cb0e27d9142c78e4e135a42fb24de3fa54
Catalin Stoicescu <stoicescu.catalinn97@gmail.com>
2020-06-11 21:48:11 +0300

    add register and login unit testing

--- src/main/java/com/quickpay/model/dto/AccountLoginRequestDTO.java
+++ src/main/java/com/quickpay/model/dto/AccountLoginRequestDTO.java
@@ -1,11 +1,12 @@
 package com.quickpay.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
-@NoArgsConstructor
+@AllArgsConstructor
 public class AccountLoginRequestDTO {
 
     @NonNull
